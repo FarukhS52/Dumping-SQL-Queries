cb3b6caa4c39c11d90f4f4fd5748917aa07e906b
Farookh Zaheer Siddiqui <129654632+FarukhS52@users.noreply.github.com>
2023-11-10 17:10:50 +0530

    Update and rename Queries-QnA to Queries-QnA.sql

--- randomQueries/Queries-QnA.sql
+++ randomQueries/Queries-QnA.sql
@@ -1,11 +1,11 @@
--- 1- Show first name and last name of patients that weight within the range of 100 to 120 (inclusive).
+-- 1- Show first name and last name of patients that weigh within the range of 100 to 120 (inclusive).
 
    select first_name, last_name from patients
    where weight between 100 and 120;
 
 
 
--- 2. Show the city, company_name, contact_name of all customers from cities which contains the letter 'L' in the city name, sorted by contact_name
+-- 2. Show the city, company_name, contact_name of all customers from cities that contain the letter 'L' in the city name, sorted by contact_name
 
    select city ,company_name, contact_name from customers
    where city  like '%L%'
@@ -18,13 +18,13 @@
    from patients ;
 
 
--- 4. Show all columns for patients who have one of the following patient_ids:<br> 1,45,534,879,1000.
+-- 4. Show all columns for patients with one of the following patient_ids:<br> 1,45,534,879,1000.
 
    select * from patients 
    where patient_id in (1, 45, 534, 879, 1000);
 
 
--- 5. Show the patient_id and the total number of admissions for patient_id 579 with count column having the alias total_admissions
+-- 5. Show the patient_id and the total number of admissions for patient_id 579 with the count column having the alias total_admissions
 
    select patient_id , count(patient_id) as total_admissions
    from admissions
