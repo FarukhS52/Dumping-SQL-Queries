1a3f9b8dbebe8dfabab76be6636d1ea803e32bab
Farookh <siddiquifarukh52@gmail.com>
2023-11-10 17:58:26 +0530

    new query

--- randomQueries/Queries-QnA.sql
+++ randomQueries/Queries-QnA.sql
@@ -38,6 +38,10 @@
    where Country = 'USA'
    order by FirstName asc;
 
-   
+-- 7. Show all the columns from admissions where the patient was admitted and discharged on the same day.
+
+
+   select * from admissions
+   where admission_date = discharge_date;   
 
 
