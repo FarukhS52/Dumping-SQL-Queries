2cd38afe112dd40793a1abe9e7e703aa446abb98
Farookh <siddiquifarukh52@gmail.com>
2023-11-16 12:07:03 +0530

    update

--- randomQueries/Queries-QnA.sql
+++ randomQueries/Queries-QnA.sql
@@ -1,96 +1,151 @@
 -- 1- Show first name and last name of patients that weigh within the range of 100 to 120 (inclusive).
 
-   select first_name, last_name from patients
-   where weight between 100 and 120;
+   SELECT 
+    first_name, last_name
+FROM
+    patients
+WHERE
+    weight BETWEEN 100 AND 120;
 
 
 
 -- 2. Show the city, company_name, contact_name of all customers from cities that contain the letter 'L' in the city name, sorted by contact_name
 
-   select city ,company_name, contact_name from customers
-   where city  like '%L%'
-   order by contact_name ASC;
+   SELECT 
+    city, company_name, contact_name
+FROM
+    customers
+WHERE
+    city LIKE '%L%'
+ORDER BY contact_name ASC;
 
 
 -- 3. Show the first_name, last_name, and height of the patient with the greatest height.
 
-   select  max( height) as height , first_name , last_name 
-   from patients ;
+   SELECT 
+    MAX(height) AS height, first_name, last_name
+FROM
+    patients;
 
 
 -- 4. Show all columns for patients with one of the following patient_ids:<br> 1,45,534,879,1000.
 
-   select * from patients 
-   where patient_id in (1, 45, 534, 879, 1000);
+   SELECT 
+    *
+FROM
+    patients
+WHERE
+    patient_id IN (1 , 45, 534, 879, 1000);
 
 
 -- 5. Show the patient_id and the total number of admissions for patient_id 579 with the count column having the alias total_admissions
 
-   select patient_id , count(patient_id) as total_admissions
-   from admissions
-   where patient_id = 579;
+   SELECT 
+    patient_id, COUNT(patient_id) AS total_admissions
+FROM
+    admissions
+WHERE
+    patient_id = 579;
    
    
 -- 6. Retrieve US customers' names and countries, ordered by first name.
 
-   select FirstName ,  Country 
-   from Customer
-   where Country = 'USA'
-   order by FirstName asc;
+   SELECT 
+    FirstName, Country
+FROM
+    Customer
+WHERE
+    Country = 'USA'
+ORDER BY FirstName ASC;
    
 -- 7. Fetch unique customer cities in alphabetical order
 
-Select Distinct(City) from Customer
-order by City ASC; 
+SELECT DISTINCT
+    (City)
+FROM
+    Customer
+ORDER BY City ASC; 
 
 
 -- 8. Show all the columns from admissions where the patient was admitted and discharged on the same day.
 
 
-   select * from admissions
-   where admission_date = discharge_date;  
+   SELECT 
+    *
+FROM
+    admissions
+WHERE
+    admission_date = discharge_date;  
    
    
 -- 9. Show all the even numbered Order_id from the orders table
 
-   select Order_id from orders
-   where MOD(Order_id,2)=0;
+   SELECT 
+    Order_id
+FROM
+    orders
+WHERE
+    MOD(Order_id, 2) = 0;
    
 -- 10. Retrieve track names and album IDs for tracks over 5 minutes
 
-   select AlbumId  ,Name 
-   from Track
-   where Milliseconds >300000
-   order by Name ASC;
+  SELECT 
+    AlbumId, Name
+FROM
+    Track
+WHERE
+    Milliseconds > 300000
+ORDER BY Name ASC;
  
 -- 11. Show the average unit price rounded to 2 decimal places, the total units in stock,
 --     total discontinued products from the products table. The column names must be average_price, total_stock, total_discontinued
 
-   SELECT
-  ROUND(AVG(unit_price), 2) AS average_price,
-  SUM(units_in_stock) AS total_stock,
-  SUM(discontinued) AS total_discontinued
-  FROM
-  products;
+   SELECT 
+    ROUND(AVG(unit_price), 2) AS average_price,
+    SUM(units_in_stock) AS total_stock,
+    SUM(discontinued) AS total_discontinued
+FROM
+    products;
   
 -- 12. Show patient_id and first_name from patients where their first_name start
 --     and ends with s and is at least 6 characters long and ordered by patient_id
 
-    select patient_id , first_name
-    from patients
-    where first_name like 's%s' And LENGTH(first_name) >=6;
+    SELECT 
+    patient_id, first_name
+FROM
+    patients
+WHERE
+    first_name LIKE 's%s'
+        AND LENGTH(first_name) >= 6;
     
 -- 13. display the number of duplicate patients based on their first_name and last_name.
 --     Make sure the column names are first_name, last_name, num_of_duplicates
 
-    Select first_name , last_name, COUNT(first_name) as num_of_duplicates
-    from patients 
-    group by first_name,last_name
-    having count(first_name)>1;
+    SELECT 
+    first_name,
+    last_name,
+    COUNT(first_name) AS num_of_duplicates
+FROM
+    patients
+GROUP BY first_name , last_name
+HAVING COUNT(first_name) > 1;
     
 -- 14. Show patient_id, diagnosis from admissions. Find patients admitted more than 1 times for the same diagnosis.
 
-   SELECT patient_id, diagnosis
-   FROM admissions
-   GROUP BY patient_id, diagnosis
-   HAVING COUNT(*) > 1;    
+   SELECT 
+    patient_id, diagnosis
+FROM
+    admissions
+GROUP BY patient_id , diagnosis
+HAVING COUNT(*) > 1;    
+   
+-- 15. Show all columns for patient_id 542's most recent admission_date.
+
+   SELECT *
+FROM admissions
+WHERE patient_id = 542
+    AND admission_date = (
+        SELECT MAX(admission_date)
+        FROM admissions
+        WHERE patient_id = 542
+    );   
